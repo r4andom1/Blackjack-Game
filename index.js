d1a61dd78680715cf8262a91cda212433086db01
r4andom1 <kim.petersson301@gmail.com>
2024-07-09 11:23:16 +0200

    Updating files
    adding some javascript to be removed later on, also added some html functionality.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+<<<<<<< Updated upstream
 console.log("Hello")
+=======
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true;
+} else {
+    console.log("You're out of the game!")
+    isAlive = false;
+}
+
+// Cash out!
+console.log(hasBlackJack)
+console.log(isAlive)
+
+>>>>>>> Stashed changes
