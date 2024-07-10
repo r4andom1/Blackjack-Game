779d48a187a4eb636dc04adc42abb70e4a78b38c
r4andom1 <kim.petersson301@gmail.com>
2024-07-10 20:11:46 +0200

    Added functions and code JS

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
+let message = "";
+let sum = 0;
+let hasBlackJack = false;
+let cards = [];
+let isAlive = false
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+console.log(cards)
+
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard
     renderGame()
+    
 }
 
 function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + ", ";
+    } 
+    
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -14,23 +37,21 @@ function renderGame() {
     }
     messageEl.textContent = message;
     sumEl.textContent = "Sum: " + sum;
-    cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1];
 }
 
 function newCard() {
-    let card = 5;
+    let card = getRandomCard()
     sum += card;
+    cards.push(card);
     renderGame()
 }
 
-let message = ""
-let firstCard = 10;
-let secondCard = 11;
-let sum = firstCard + secondCard;
-let hasBlackJack = false;
-let isAlive = true;
-let cards = [firstCard, secondCard];
-
-let messageEl = document.getElementById("message-el");
-let sumEl = document.getElementById("sum-el");
-let cardsEl = document.getElementById("cards-el");
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random() * 13 + 1)
+    if (randomCard > 10) {
+        return 10;
+    } else if (randomCard === 1) {
+        return 11;
+    }
+    return randomCard;
+}
